f76be5a546d52e8430f91cc5d1dc3c970fcfb6c5
CGM <mrmouse.guru@gmail.com>
2025-07-04 12:21:58 +0700

    initialize Project

---
+++ src/association/ClassA.java
@@ -0,0 +1,15 @@
+package association;
+
+public class ClassA {
+	
+	private String name;
+	
+	ClassA(){
+		System.out.println("Đang đẻ đối tượng a");
+	}
+	
+	public void m1() {
+		System.out.println("m1 ...");
+	}
+
+}
---
+++ src/association/Main.java
@@ -0,0 +1,14 @@
+package association;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//new <tên hàm khởi tạo đối tượng - constructor> 
+		new ClassA();
+		
+		System.out.println("đã tạo đối tượng a");
+
+	}
+
+}
